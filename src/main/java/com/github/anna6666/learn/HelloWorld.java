963847615793cf72fa0f98a428724bd56e4bca54
Anna6666 <evgenij-rastorguev@yandex.ru>
2016-12-09 18:16:59 +0300

    build configuration

---
+++ src/main/java/com/github/anna6666/learn/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.github.anna6666.learn;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        // Это комментарий. Он ене влияет на программу. Просто помогает читать.
+        System.out.println("Hi Hitler"); // Это код который не который
+    }
+
+}
