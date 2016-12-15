6d7b5fd7c3b801cc0f393a8941749194fde1204d
Anna6666 <evgenij-rastorguev@yandex.ru>
2016-12-15 21:09:48 +0300

    .\src

--- src/main/java/com/github/anna6666/learn/HelloWorld.java
+++ src/main/java/com/github/anna6666/learn/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         // Это комментарий. Он ене влияет на программу. Просто помогает читать.
-        System.out.println("Hi Hitler"); // Это код который не который
+        System.out.println("Hi Hitler"); // Это код который не который не обращаем внимания
+        //хочуспатьхочуспатьхочуспатьхочуспатьхочуспать
     }
 
 }
---
+++ src/test/java/com/github/anna6666/learn/test/TestDataTypes.java
@@ -0,0 +1,50 @@
+package com.github.anna6666.learn.test;
+
+import com.github.anna6666.learn.HelloWorld;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+
+/**
+
+ */
+public class TestDataTypes extends TestCase {
+
+
+    public void testMain() {
+        HelloWorld.main(null);
+    }
+
+    public void test1() {
+        assertEquals(1, 2);
+    }
+
+    public void testPrint() {
+        String hi = "Hi2";
+        System.out.print(hi);
+    }
+
+    public void testIntSimple() {
+        int a = 10;
+        int b = 7;
+        int sum = a + b;
+        System.out.print(sum);
+    }
+
+    public void testBoolSimple() {
+        boolean a = true;
+        boolean f = false;
+
+        if (f) {
+            System.out.println("a is True");
+        } else {
+            System.out.println("a is not True");
+        }
+    }
+
+    public void testChar() {
+        char c = 'x';
+        System.out.println(c);
+    }
+
+}
